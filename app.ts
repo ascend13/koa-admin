6637f17e387275df760a233c8961e045108a01d8
WSH <384035991@qq.com>
2021-01-12 00:18:04 +0800

    测试

--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
-const koa = require("koa");
+import { Context } from "koa";
+
+const Koa = require("koa");
+const Router = require("koa-router")
+
+const app = new Koa();
+const router = new Router();
+
+// 路由
+router.get("/", async (ctx: Context) => {
+    ctx.body = {
+        msg: 'koa接口返回'
+    }
+})
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`启动端口${port}`)
+})
